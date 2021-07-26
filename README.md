28334e4fc3225f583376c3ab7d9a42e40b43a1eb
Daniel Gultsch <daniel@gultsch.de>
2021-07-26 17:01:05 +0200

    initial commit. stub project w/ gradle + shadow jar

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ltt.rs Push Server
+
+### Compile
+```shell
+./gradlew shadowJar
+```
+
+### Run
+```shell
+java -jar build/libs/lttrs-push-server-0.1-all.jar
+```
---
+++ src/main/java/rs/ltt/push/PushServer.java
@@ -0,0 +1,9 @@
+package rs.ltt.push;
+
+public class PushServer {
+
+    public static void main(final String... args) {
+        System.out.println("Hello World");
+    }
+
+}
